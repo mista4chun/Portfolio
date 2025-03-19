1e5f68f3d312954f1de18c505ff40110f8b845b0
Fortune Samson <mr4chun4u@gmail.com>
2025-03-19 20:30:00 +0100

    Added more animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,12 @@ import { FaJs } from "react-icons/fa";
 import { RiSupabaseFill } from "react-icons/ri";
 import { BiLogoTailwindCss } from "react-icons/bi";
 import { Link } from "react-router-dom";
-
+import { motion } from "motion/react";
 function Home() {
   return (
-    
     <section className="bg-[#1e1e1e]">
       <PageNav type="horizontal" />
-      <div className="mx-auto mt-36 md:flex max-w-2xl items-center justify-between  pb-24 md:max-w-full md:px-12 px-6">
+      <div className="mx-auto mt-36 max-w-2xl items-center justify-between px-6 pb-24 md:flex md:max-w-full md:px-12">
         <article>
           <h1 className="mb-2 text-3xl font-semibold tracking-wider md:text-6xl">
             Mista4chun
@@ -21,9 +20,19 @@ function Home() {
             text="Software Developer with React."
             speed={100}
             lastWordColor="#01b6cd"
-           className="text-lg  md:text-3xl"
+            className="text-lg md:text-3xl"
           />
-          <div className="mt-8 hidden gap-8 text-center tracking-wider md:flex">
+          <motion.div
+            initial={{ y: 100, opacity: 0 }} // Start further below for more impact
+            animate={{ y: 0, opacity: 1 }} // Slide to position
+            transition={{
+              type: "spring",
+              stiffness: 200, // Higher stiffness for a snappier effect
+              damping: 8, // Lower damping to allow more bounce
+              duration: 0.9, // Smooth transition duration
+            }}
+            className="mt-8 hidden gap-8 text-center tracking-wider md:flex"
+          >
             <div className="flex flex-col items-center">
               <IoLogoReact className="h-12 w-12 text-[#01b6cd]" />
               <p>React</p>
@@ -48,9 +57,7 @@ function Home() {
               <SiNextdotjs className="h-12 w-12 text-[#fff] text-opacity-100" />
               <p>Next.js</p>
             </div>
-
-          
-          </div>
+          </motion.div>
         </article>
         <article>
           <img
@@ -60,29 +67,28 @@ function Home() {
           />
         </article>
 
-        <article className="flex items-center gap-4 mt-4 text-sm md:hidden">
-              <IoLogoReact className="h-10 w-10 text-[#01b6cd]" />
+        <article className="mt-4 flex items-center gap-4 text-sm md:hidden">
+          <IoLogoReact className="h-10 w-10 text-[#01b6cd]" />
 
-              <BiLogoTailwindCss className="h-10 w-10 text-[#0088cc]" />
+          <BiLogoTailwindCss className="h-10 w-10 text-[#0088cc]" />
 
-              <FaJs className="h-10 w-10 text-[#ffca28]" />
+          <FaJs className="h-10 w-10 text-[#ffca28]" />
 
-              <RiSupabaseFill className="h-10 w-10 text-[#40d08f]" />
+          <RiSupabaseFill className="h-10 w-10 text-[#40d08f]" />
 
-              <SiNextdotjs className="h-10 w-10 text-[#fff] text-opacity-100" />
-            </article>
+          <SiNextdotjs className="h-10 w-10 text-[#fff] text-opacity-100" />
+        </article>
       </div>
-      
-      <div className="mx-auto  -mt-5 flex max-w-2xl items-center space-x-8 px-8 md:hidden">
-        <button className="bg-[#01b6cd] px-4 py-2 transition-all duration-150 hover:-translate-y-0.5 hover:shadow-lg text-gray-800 font-semibold">
-     
+
+      <div className="mx-auto -mt-5 flex max-w-2xl items-center space-x-8 px-8 md:hidden">
+        <button className="bg-[#01b6cd] px-4 py-2 font-semibold text-gray-800 transition-all duration-150 hover:-translate-y-0.5 hover:shadow-lg">
           <Link to="/projects">My Work</Link>
         </button>
-        <button className="border border-[#01b6cd] px-4 py-2 transition-all duration-150 hover:-translate-y-0.5 hover:shadow-lg font-semibold">
+        <button className="border border-[#01b6cd] px-4 py-2 font-semibold transition-all duration-150 hover:-translate-y-0.5 hover:shadow-lg">
           <Link to="/resume">My Resume</Link>
         </button>
       </div>
-      <h2 className="text-center font-bigFont hidden md:block  font-semibold text-slate-700/10 text-9xl">
+      <h2 className="hidden text-center font-bigFont text-9xl font-semibold text-slate-700/10 md:block">
         I build magic!
       </h2>
     </section>
